880893450bf463a42101e1011cf07cf7c71625e5
ko-morishita-clinks <ko-morishita@clinks.jp>
2025-09-19 13:55:01 +0900

    feat: test mail

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -5,9 +5,12 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
 use App\Models\Product;
 use App\Models\PrimaryCategory;
 use App\Models\Stock;
+use App\Mail\TestMail;
+use App\Jobs\SendThanksMail;
 
 class ItemController extends Controller
 {
@@ -29,7 +32,10 @@ class ItemController extends Controller
     }
         
     public function index(Request $request)
-    {  
+    {
+        Mail::to('test@example.com')
+        ->send(new TestMail());
+        
         $categories = PrimaryCategory::with('secondary')
         ->get();
 
